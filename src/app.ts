850117909db47c911f9db883e81db5093dbcf22e
dinitrogen-tetroxide <andrewlulu2012@gmail.com>
2021-01-20 15:40:48 +1000

    More CRUD work

--- src/app.ts
+++ src/app.ts
@@ -13,28 +13,52 @@ app.use(bodyParser.json({
     }
 }));
 
+// Get an item's recipe or uses
 app.get('/crafting-table-recipe/:item', async (req, res) => {
     try {
-        const recipe = await CraftingTableRecipe.findOne({
-            where: {
-                item: req.params.item
+        const type = req.query.type;
+
+        if (type === 'recipe') {
+            const recipe = await CraftingTableRecipe.findOne({
+                where: {
+                    item: req.params.item
+                }
+            });
+            
+            if (recipe) {
+                res.json(recipe);
+            } else {
+                res.status(404).send({message: "Recipe not found"});
             }
-        });
-        
-        if (recipe) {
-            res.json(recipe);
+        } else if (type === 'uses') {
+            /* Broken, figure out a way to do LIKE operator on table
+            const regex = new RegExp(`.*${req.params.item}.*`);
+
+            const recipes = await CraftingTableRecipe.find({
+                where: {
+                    slots: regex
+                }
+            });
+
+            if (recipes) {
+                res.json(recipes);
+            } else {
+                res.status(404).send({message: "Recipes not found"});
+            }*/
         } else {
-            res.status(404).send({message: "Recipe not found"});
+            throw(Error);
         }
+
     } catch (error) {
         res.status(500).send({message: "Unexpected error"})
     }
 });
 
+// Create new recipe in table
 app.post('/crafting-table-recipe', async (req, res) => {
     const recipe = new CraftingTableRecipe();
-    recipe.item = req.body.item ? req.body.item : recipe.item;
-    recipe.slots = req.body.slots ? req.body.slots : recipe.slots;
+    recipe.item = req.body.item;
+    recipe.slots = JSON.stringify(req.body.slots);
     try {
         await recipe.save();
         res.send(recipe);
@@ -42,5 +66,16 @@ app.post('/crafting-table-recipe', async (req, res) => {
         res.status(500).send({message: "Unable to save recipe to database"});
     }
 });
+
+// Update recipe in table
+app.put('/crafting-table-recipe/:name', async (req, res) => {
+    const recipe = CraftingTableRecipe.find({
+        where: {
+            name: req.params.name
+        }
+
+        
+    })
+})
 
 export {app};
--- src/db/models/crafting-table-recipe.model.ts
+++ src/db/models/crafting-table-recipe.model.ts
@@ -1,5 +1,7 @@
 import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 
+// TODO: implement recipe output counters (ie 9x of item per recipe)
+
 @Entity('crafting-table-recipe')
 export class CraftingTableRecipe extends BaseEntity {
     @PrimaryGeneratedColumn()
